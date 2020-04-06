8f06ceb79b315b8dcfac601201632cf235503510
David <david.cabello.info@gmail.com>
2020-04-06 13:39:17 +0200

    Este es el primer commit para clonar un proyecto de Eclipse en un repositorio de GitHub que tengo en local.

---
+++ src/es/studium/PatronMVC/Principal.java
@@ -0,0 +1,13 @@
+package es.studium.PatronMVC;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Modelo modelo;
+		Vista vista;
+		new Controlador(modelo, vista);
+
+	}
+
+}
---
+++ src/es/studium/PatronMVC/Vista.java
@@ -0,0 +1,5 @@
+package es.studium.PatronMVC;
+
+public class Vista {
+
+}
