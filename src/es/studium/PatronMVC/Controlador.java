41c7ca62ea337bdc9b85d195af7fcb498eb09791
David <david.cabello.info@gmail.com>
2020-04-15 20:09:57 +0200

    commit creacion métodos mostrar, modificar, crear

--- src/es/studium/PatronMVC/Controlador.java
+++ src/es/studium/PatronMVC/Controlador.java
@@ -5,8 +5,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-
-
 public class Controlador extends WindowAdapter implements ActionListener {
 
 	Vista objVista = null;
@@ -22,6 +20,8 @@ public class Controlador extends WindowAdapter implements ActionListener {
 		objVista.frmEmpresa.addWindowListener(this);
 		objVista.dlgNuevo.addWindowListener(this);
 		objVista.dlgModificar.addWindowListener(this);
+		objVista.dlgSelectMod.addWindowListener(this);
+		objVista.dlgSeguro.addWindowListener(this);
 		objVista.dlgEliminar.addWindowListener(this);
 		objVista.dlgConsultar.addWindowListener(this);
 
@@ -36,13 +36,14 @@ public class Controlador extends WindowAdapter implements ActionListener {
 		objVista.btnConsultar.addActionListener(this);
 		objVista.btnEliminar.addActionListener(this);
 		objVista.btnModificar.addActionListener(this);
-
-		// Botones cancelar
 		objVista.btnCancelarNuevo.addActionListener(this);
 		objVista.btnCancelarModificar.addActionListener(this);
-		objVista.btnCancelarEliminar.addActionListener(this);
-		objVista.btnCancelarConsultar.addActionListener(this);
-
+		objVista.btnLimpiarEliminar.addActionListener(this);
+		objVista.btnImprimirConsultar.addActionListener(this);
+		objVista.btnLimpiar.addActionListener(this);
+		objVista.btnEditar.addActionListener(this);
+		objVista.btnSi.addActionListener(this);
+		objVista.btnNo.addActionListener(this);
 		// Añadimos los objetos
 
 	}
@@ -64,6 +65,14 @@ public class Controlador extends WindowAdapter implements ActionListener {
 		} else if (objVista.dlgConsultar.isActive()) {
 
 			objVista.dlgConsultar.setVisible(false);
+
+		} else if (objVista.dlgSelectMod.isActive()) {
+
+			objVista.dlgSelectMod.setVisible(false);
+
+		} else if (objVista.dlgSeguro.isActive()) {
+
+			objVista.dlgSeguro.setVisible(false);
 
 		} else {
 
@@ -100,7 +109,59 @@ public class Controlador extends WindowAdapter implements ActionListener {
 
 		} else if (objVista.mniModificar.equals(event.getSource())) {
 
+			objVista.listaEmpleadosModificar = objModelo.SelectEmpleado(objModelo.GestorConexion(),
+					objVista.listaEmpleadosModificar);
+			
+			objModelo.CerrarConexion();
+			
+			objVista.dlgSelectMod.setVisible(true);
+			
+			
+
+		} else if (objVista.btnEditar.equals(event.getSource())) {
+
+			int id = Integer.parseInt(objVista.listaEmpleadosModificar.getSelectedItem().split("-")[0]);
+
+			objModelo.mostrarDatos(objModelo.GestorConexion(), id, objVista.txtEmpleadoModificar,
+					objVista.txtExtensionModificar, objVista.txtFechaNacModificar, objVista.txtFechaIngModificar,
+					objVista.txtSalarioModificar, objVista.txtComisionModificar, objVista.txtHijosModificar,
+					objVista.txtNombreModificar);
+			
+			objModelo.CerrarConexion();
+
 			objVista.dlgModificar.setVisible(true);
+			
+			
+		} else if (objVista.btnLimpiar.equals(event.getSource())) {
+
+			objVista.listaEmpleadosModificar.select(0);
+
+		} else if (objVista.btnModificar.equals(event.getSource())) {
+
+			objVista.dlgModificar.setVisible(false);
+			
+			objVista.dlgSeguro.setVisible(true);
+
+		} else if (objVista.btnCancelarModificar.equals(event.getSource())) {
+
+			objVista.dlgModificar.setVisible(false);
+
+		} else if (objVista.btnSi.equals(event.getSource())) {
+
+			objModelo.modificarEmpleado(objModelo.GestorConexion(), objVista.txtEmpleadoModificar, objVista.txtExtensionModificar, objVista.txtFechaNacModificar, objVista.txtFechaIngModificar, objVista.txtSalarioModificar, objVista.txtComisionModificar, objVista.txtHijosModificar, objVista.txtNombreModificar);
+
+			objVista.dlgModificar.setVisible(false);
+			
+			objVista.dlgSeguro.setVisible(false);
+			
+			objModelo.CerrarConexion();
+			
+			objVista.dlgSelectMod.setVisible(true);			
+			
+			
+		} else if (objVista.btnNo.equals(event.getSource())) {
+
+			objVista.dlgSeguro.setVisible(false);
 
 		} else if (objVista.btnConsultar.equals(event.getSource())) {
 
@@ -115,40 +176,57 @@ public class Controlador extends WindowAdapter implements ActionListener {
 			objVista.dlgConsultar.setVisible(true);
 
 		} else if (objVista.mniEliminar.equals(event.getSource())) {
-			
-			
-			objVista.listaEmpleadoEliminar = objModelo.SelectEmpleado(objModelo.GestorConexion(), objVista.listaEmpleadoEliminar);	
-			
+
+			objVista.listaEmpleadoEliminar = objModelo.SelectEmpleado(objModelo.GestorConexion(),
+					objVista.listaEmpleadoEliminar);
+
+			objModelo.CerrarConexion();
 			
 			objVista.dlgEliminar.setVisible(true);
 
-		}else if (objVista.btnEliminar.equals(event.getSource())) {
-			
-			//Hacemos un split para aislar el dato idEmpleado
+		} else if (objVista.btnEliminar.equals(event.getSource())) {
+
+			// Hacemos un split para aislar el dato idEmpleado
 			String[] empleado = objVista.listaEmpleadoEliminar.getSelectedItem().split("-");
-			
-			//Guardamos el resultado que nos devuelve el método BorrarEmpleado en la cvariable respuesta
+
+			// Guardamos el resultado que nos devuelve el método BorrarEmpleado en la
+			// cvariable respuesta
 			int respuesta = objModelo.BorrarEmpleado(objModelo.GestorConexion(), Integer.parseInt(empleado[0]));
-			
-			// Mostramos resultado 
-			if (respuesta == 0)
-			{
+
+			// Mostramos resultado
+			if (respuesta == 0) {
 				System.out.println("Borrado de empleado correcto");
-				
-			} else
-			{
+
+			} else {
 				System.out.println("Error en Borrado de empleado");
 			}
-				objModelo.CerrarConexion();
 			
+			objModelo.CerrarConexion();
+
+		} else if (objVista.btnLimpiarEliminar.equals(event.getSource())) {
+
+			objVista.listaEmpleadoEliminar.select(0);
+
+		} else if (objVista.btnModificar.equals(event.getSource())) {
+
+			// Hacemos un split para aislar el dato idEmpleado
+			String[] empleado = objVista.listaEmpleadosModificar.getSelectedItem().split("-");
+
+			// Guardamos el resultado que nos devuelve el método BorrarEmpleado en la
+			// cvariable respuesta
+			int respuesta = objModelo.BorrarEmpleado(objModelo.GestorConexion(), Integer.parseInt(empleado[0]));
+
+			// Mostramos resultado
+			if (respuesta == 0) {
+				System.out.println("Borrado de empleado correcto");
+
+			} else {
+				System.out.println("Error en Borrado de empleado");
+			}
 			
-		}else if (objVista.btnCancelarEliminar.equals(event.getSource())) {
-			
-				objModelo.CerrarConexion();
-				objVista.dlgEliminar.setVisible(false);
-				
+			objModelo.CerrarConexion();
+
 		}
 
 	}
-	
 }
--- src/es/studium/PatronMVC/Modelo.java
+++ src/es/studium/PatronMVC/Modelo.java
@@ -2,6 +2,7 @@ package es.studium.PatronMVC;
 
 import java.awt.Choice;
 import java.awt.TextArea;
+import java.awt.TextField;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -11,7 +12,6 @@ import java.sql.Statement;
 public class Modelo {
 
 	Connection connect = null;
-	
 
 	public void CerrarConexion() {
 
@@ -40,13 +40,13 @@ public class Modelo {
 			Class.forName(driver);
 			// Establecer la conexión con la base de datos
 			connect = DriverManager.getConnection(url, login, pass);
-			
+
 			if (connect != null) {
 
 				System.out.println(" Conectado correctamente a la base de datos");
 
 			}
-			
+
 		} catch (SQLException ex) {
 
 			System.out.println("ERROR: No se ha podido conectar con la base de datos" + ex.getMessage());
@@ -59,113 +59,175 @@ public class Modelo {
 		return connect;
 
 	}
-	
-	public int insertar(Connection con, String tabla,  String extensionEmpleado, String fechaNacimientoEmpleado, 
-			String fechaIngresoEmpleado, String salarioEmpleado, String comisionEmpleado, String hijosEmpleado, String nombreEmpleado)
-	{
+
+	public int insertar(Connection con, String tabla, String extensionEmpleado, String fechaNacimientoEmpleado,
+			String fechaIngresoEmpleado, String salarioEmpleado, String comisionEmpleado, String hijosEmpleado,
+			String nombreEmpleado) {
 		int respuesta = 0;
-		
-		try
-		{
+
+		try {
 			// Creamos un STATEMENT para una consulta SQL INSERT.
 			Statement statement = con.createStatement();
-			
-			String cadenaSQL = "INSERT INTO " + tabla + " VALUES (null, null ,'" 
-				+ extensionEmpleado +  " ', '" + fechaNacimientoEmpleado + "', '" 
-				+ fechaIngresoEmpleado + "', '" + salarioEmpleado + "', '" + comisionEmpleado + "', '" 
-				+ hijosEmpleado + "', '" + nombreEmpleado + "');";
-				System.out.println(cadenaSQL);
-				statement.executeUpdate(cadenaSQL);			
-			
-				statement.close();
-		} catch (SQLException ex)
-		{
+
+			String cadenaSQL = "INSERT INTO " + tabla + " VALUES (null, null ,'" + extensionEmpleado + " ', '"
+					+ fechaNacimientoEmpleado + "', '" + fechaIngresoEmpleado + "', '" + salarioEmpleado + "', '"
+					+ comisionEmpleado + "', '" + hijosEmpleado + "', '" + nombreEmpleado + "');";
+			System.out.println(cadenaSQL);
+			statement.executeUpdate(cadenaSQL);
+
+			statement.close();
+		} catch (SQLException ex) {
 			System.out.println("ERROR:al hacer un Insert");
 			ex.printStackTrace();
 			respuesta = 1;
-			
+
 		}
 		return respuesta;
 	}
-	
+
 	public int BorrarEmpleado(Connection con, int idEmpleado)
-	
+
 	{
 		int respuesta = 0;
 		String sql = "DELETE FROM empleados WHERE idEmpleado = " + idEmpleado;
 		System.out.println(sql);
-		
-		
-		try
-		{
+
+		try {
 			// Creamos un STATEMENT para una consulta SQL INSERT.
 			Statement sta = con.createStatement();
 			sta.executeUpdate(sql);
 			sta.close();
-		} catch (SQLException ex)
-		{
+		} catch (SQLException ex) {
 			System.out.println("ERROR:al intentar borrar el empleado " + idEmpleado);
 			ex.printStackTrace();
 			respuesta = 1;
 		}
 		return respuesta;
 	}
-	public void ConsultaEmpleados(Connection con, TextArea areaTxt)
-	{
+
+	public void ConsultaEmpleados(Connection con, TextArea areaTxt) {
 		String statement = "SELECT * FROM empleados";
-		try
-		{
+		try {
 			// CREAR UN STATEMENT PARA UNA CONSULTA SELECT
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(statement);
-			while (rs.next())
-			{
-				//String[] Servicio=listaServicios.getSelectedItem().split("-");
-				if (areaTxt.getText().length() == 0)
-				{
+			while (rs.next()) {
+				// String[] Servicio=listaServicios.getSelectedItem().split("-");
+				if (areaTxt.getText().length() == 0) {
 					areaTxt.setText(rs.getInt("idEmpleado") + "-" + rs.getString("idDepartamentoFK") + ", "
 							+ rs.getString("extensionEmpleado") + ", " + rs.getString("fechaNacimientoEmpleado") + ", "
 							+ rs.getString("fechaIngresoEmpleado") + ", " + rs.getString("salarioEmpleado") + ", "
 							+ rs.getString("comisionEmpleado") + ", " + rs.getString("hijosEmpleado") + ", "
 							+ rs.getString("nombreEmpleado"));
-				} else
-				{
+				} else {
 					areaTxt.setText(areaTxt.getText() + "\n" + rs.getInt("idEmpleado") + "-"
 							+ rs.getString("idDepartamentoFK") + ", " + rs.getString("extensionEmpleado") + ", "
-							+ rs.getString("fechaNacimientoEmpleado") + ", " + rs.getString("fechaIngresoEmpleado") + ", "
-							+ rs.getString("salarioEmpleado") + ", " + rs.getString("comisionEmpleado")+ ", " 
-							+ rs.getString("hijosEmpleado") + ", " + rs.getString("nombreEmpleado"));			}
+							+ rs.getString("fechaNacimientoEmpleado") + ", " + rs.getString("fechaIngresoEmpleado")
+							+ ", " + rs.getString("salarioEmpleado") + ", " + rs.getString("comisionEmpleado") + ", "
+							+ rs.getString("hijosEmpleado") + ", " + rs.getString("nombreEmpleado"));
+				}
 			}
 			rs.close();
 			stmt.close();
-		} catch (SQLException ex)
-		{
+		} catch (SQLException ex) {
 			System.out.println("ERROR:al realizar la consulta");
 			ex.printStackTrace();
 		}
 	}
-	
-		public Choice SelectEmpleado (Connection con, Choice listaEmpleadoEliminar) {
-			
-				
-			String statement = "SELECT * FROM empleados";
-			try
-			{	
-				
-				// CREAR UN STATEMENT PARA UNA CONSULTA SELECT
-				Statement stmt = con.createStatement();
-				ResultSet rs = stmt.executeQuery(statement);
-				while (rs.next())
-				{
-					listaEmpleadoEliminar.add(rs.getInt("idEmpleado")+ "- " + rs.getString("nombreEmpleado") + ", " + rs.getString("extensionEmpleado") + ", "
-							
-							+ rs.getString("salarioEmpleado")); 
-					
-				}
-			}catch (SQLException error) {
-				
-				System.out.println("Se ha producido un error");
+
+	public Choice SelectEmpleado(Connection con, Choice listaEmpleado) {
+
+		String statement = "SELECT * FROM empleados";
+		try {
+
+			// CREAR UN STATEMENT PARA UNA CONSULTA SELECT
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(statement);
+			while (rs.next()) {
+				listaEmpleado.add(rs.getInt("idEmpleado") + "- " + rs.getString("nombreEmpleado") + ", "
+						+ rs.getString("extensionEmpleado") + ", "
+
+						+ rs.getString("salarioEmpleado"));
+
 			}
-			return listaEmpleadoEliminar;
+		} catch (SQLException error) {
+
+			System.out.println("Se ha producido un error");
 		}
+		return listaEmpleado;
+	}
+
+	public void mostrarDatos(Connection con, int empleado, TextField txtNumeroModificar,
+			TextField txtExtensionModificar, TextField txtFechaNacModificar, TextField txtFechaIngModificar,
+			TextField txtSalarioModificar, TextField txtComisionModificar, TextField txtHijosModificar,
+			TextField txtNombreModificar) {
+		String sqlSelect = "SELECT * FROM empleados WHERE idEmpleado = " + empleado;
+		try {
+
+			// Creamos un STATEMENT para una consulta SQL.
+			Statement sta = con.createStatement();
+			ResultSet rs = sta.executeQuery(sqlSelect);
+			while (rs.next()) {
+				String stringEmpleado = rs.getString("idEmpleado");
+				/* String stringHabitacion = rs.getString(habitacion); */
+				txtNumeroModificar.setText(stringEmpleado);
+				txtNumeroModificar.setEnabled(false);
+				String stringExtension = rs.getString("extensionEmpleado");
+				txtExtensionModificar.setText(stringExtension);
+				String stringFechaNac = rs.getString("fechaNacimientoEmpleado");
+				txtFechaNacModificar.setText(stringFechaNac);
+				String stringFechaIng = rs.getString("fechaIngresoEmpleado");
+				txtFechaIngModificar.setText(stringFechaIng);
+				String stringSalario = rs.getString("salarioEmpleado");
+				txtSalarioModificar.setText(stringSalario);
+				String stringComision = rs.getString("comisionEmpleado");
+				txtComisionModificar.setText(stringComision);
+				String stringHijos = rs.getString("hijosEmpleado");
+				txtHijosModificar.setText(stringHijos);
+				String stringNombre = rs.getString("nombreEmpleado");
+				txtNombreModificar.setText(stringNombre);
+
+			}
+			sta.close();
+		} catch (SQLException ex) {
+			System.out.println("ERROR:al hacer el Select");
+			ex.printStackTrace();
+		}
+
+	}
+	public void modificarEmpleado (Connection con, TextField txtNumeroModificar,
+			TextField txtExtensionModificar, TextField txtFechaNacModificar, TextField txtFechaIngModificar,
+			TextField txtSalarioModificar, TextField txtComisionModificar, TextField txtHijosModificar,
+			TextField txtNombreModificar) {
+		
+		int id = Integer.parseInt(txtNumeroModificar.getText());
+		String extension = txtExtensionModificar.getText();
+		String fechaNac = txtFechaNacModificar.getText();
+		String fechaIng = txtFechaIngModificar.getText();
+		String salario = txtSalarioModificar.getText();
+		String comision = txtComisionModificar.getText();
+		String hijos = txtHijosModificar.getText();
+		String nombre = txtNombreModificar.getText();			
+		
+		// Ejecutar el UPDATE
+		String sqlUpdate = "UPDATE empleados SET extensionEmpleado = '" + extension 
+				+ "', fechaNacimientoEmpleado = '" + fechaNac + "', fechaIngresoEmpleado = '" 
+				+ fechaIng + "', salarioEmpleado = '" + salario + "', comisionEmpleado = '" 
+				+ comision + "', hijosEmpleado = '" + hijos+ "', nombreEmpleado = '" + nombre
+				+ "'  WHERE idEmpleado = '" + id + "'";
+		//System.out.println(sqlUpdate);
+		
+		try
+		{
+			// CREAR UN STATEMENT PARA UNA CONSULTA SELECT
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate(sqlUpdate);
+			stmt.close();
+		} catch (SQLException ex)
+		{
+			System.out.println("ERROR:al modificar");
+			ex.printStackTrace();
+		}
+		
+	}
 }
